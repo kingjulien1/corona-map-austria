06c207a0aa3f66dfbb824adbe1e7e88811941dd6
julian-blaschke <julianblaschke@icloud.com>
2020-05-01 18:42:26 +0200

    added repo and source links

--- corona-map-app/src/components/App.js
+++ corona-map-app/src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Map from "./Map";
-import { Layout, PageHeader, Spin } from "antd";
+import { PageHeader, Spin, Tooltip, Layout } from "antd";
 import { useCasesPerDistrict } from "../hooks";
+import { GithubOutlined, AreaChartOutlined } from "@ant-design/icons";
 
 function App() {
   let { cases, loading } = useCasesPerDistrict();
@@ -11,8 +12,29 @@ function App() {
         title="Corona Map"
         subTitle="aktuelle Fälle je Bezirk"
         style={{ zIndex: 5, backgroundColor: "#ffffff8c" }}
+        extra={[
+          <Tooltip title="zum Repository" placement="bottomLeft">
+            <a href="https://github.com/julian-blaschke/corona-map-austria">
+              <GithubOutlined></GithubOutlined>
+            </a>
+          </Tooltip>,
+          <Tooltip
+            title="zur Datenquelle (Österreichisches COVID-19 Open Data Informationsportal)"
+            placement="bottomLeft"
+          >
+            <a href="https://www.data.gv.at/covid-19/">
+              <AreaChartOutlined />
+            </a>
+          </Tooltip>,
+        ]}
       ></PageHeader>
-      {loading ? <Spin></Spin> : <Map data={cases}></Map>}
+      {loading ? (
+        <div style={{ width: "100%", textAlign: "center" }}>
+          <Spin></Spin>
+        </div>
+      ) : (
+        <Map data={cases}></Map>
+      )}
     </div>
   );
 }
