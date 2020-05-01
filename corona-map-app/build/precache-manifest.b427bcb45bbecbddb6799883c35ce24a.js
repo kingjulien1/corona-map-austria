27fac927be5744b58c1642bdb4ab38984276ed02
julian-blaschke <julianblaschke@icloud.com>
2020-05-01 18:45:27 +0200

    finished build for production

---
+++ corona-map-app/build/precache-manifest.b427bcb45bbecbddb6799883c35ce24a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f298aaed51d01a49277b6318d865adc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47602e2fea1e37da98b6",
+    "url": "/static/css/2.a348e988.chunk.css"
+  },
+  {
+    "revision": "47e65d0c04f478b2eee7",
+    "url": "/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "47602e2fea1e37da98b6",
+    "url": "/static/js/2.cdc529f4.chunk.js"
+  },
+  {
+    "revision": "1bec9e26b5930348ac2a2baaf836db68",
+    "url": "/static/js/2.cdc529f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e65d0c04f478b2eee7",
+    "url": "/static/js/main.a0024035.chunk.js"
+  },
+  {
+    "revision": "db922c6b63eafb2a681a",
+    "url": "/static/js/runtime-main.f1afe241.js"
+  }
+]);
